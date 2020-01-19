81b5794618c82a3eb9ab45f952d2a7d405f3cc5a
Tang-tang Zhou <tangtang.zhou@mail.polimi.it>
2020-01-19 17:30:56 +0100

    Fix train model

--- train_model.py
+++ train_model.py
@@ -12,13 +12,14 @@ if __name__ == '__main__':
     batch_size = 32
     img_h = 480
     img_w = 320
-    epochs = 5
+    epochs = 4
     tf.random.set_seed(seed)
 
-    data_path = "data/"
-    dataset_vqa_path = os.path.join(data_path, "dataset_vqa")
-    train_questions_path = os.path.join(dataset_vqa_path, "train_data.json")
-    train_image_dir = os.path.join(dataset_vqa_path, "train")
+    train_questions_path = "data/dataset_vqa/train_data.json"
+    test_questions_path = "data/dataset_vqa/test_data.json"
+    train_image_dir = "data/dataset_vqa/train"
+    test_image_dir = "data/dataset_vqa/test"
+
     tokenizer = CustomTokenizer(train_questions_path)
     train_dataset, valid_dataset, train_samples, valid_samples = read_train_valid_data(train_questions_path,
                                                                                        train_image_dir,
@@ -34,8 +35,6 @@ if __name__ == '__main__':
     model.fit(x=train_dataset, epochs=epochs, steps_per_epoch=math.ceil(train_samples / batch_size),
               validation_data=valid_dataset, validation_steps=math.ceil(valid_samples / batch_size))
 
-    test_questions_path = os.path.join(dataset_vqa_path, "test_data.json")
-    test_image_dir = os.path.join(dataset_vqa_path, "test")
     test_dataset, questions_list = read_test_data(test_questions_path, test_image_dir, img_h=img_h, img_w=img_w,
                                                   tokenizer=tokenizer, batch_size=batch_size)
     predict_submissions(model, test_dataset, questions_list, batch_size)
